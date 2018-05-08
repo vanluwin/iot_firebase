1ee8a6d6f303bfceb2438334b6f7e7527ebc03a9
vanluwin <richardsonsantiago@ufrn.edu.br>
2018-05-08 19:04:24 -0300

    update

--- code/brightness_control.py
+++ code/brightness_control.py
@@ -1,23 +1,18 @@
-import time, math, machine
+import machine
 
 # Definindo as portas que estão sendo utilizadas 
-led = machine.PWM(machine.Pin(2), freq=1000)
+led = machine.PWM(machine.Pin(15))
 pot = machine.ADC(0)
-btn = machine.Pin(4, machine.Pin.IN)
-
-# função para controlar os pulsos de intensidade
-def pulse(l, t):
-    for i in range(20):
-        l.duty(int(math.sin(i / 10 * math.pi) * 500 + 500))
-        time.sleep_ms(t)
+#btn = machine.Pin(4, machine.Pin.IN)
+btn = 1
 
 while True:
-    if(btn.value()):
-        pulse(led, 1024)
+    if btn:
+        led.duty(1024)
     else:
-        pulse(led, 0)
+        led.duty(0)
 
-    if(btn.value()):
-        pulse(led, pot.read())
+    if(btn):
+        led.duty(pot.read())
     else:
         pass
--- code/pub_states.py
+++ code/pub_states.py
@@ -1,34 +1,30 @@
-import time, math, machine, uFirebase
+import machine, uFirebase
 
-db = uFirebase.uFirebase('https://micro-py.firebaseio.com')
+fb = uFirebase.uFirebase('https://micro-py.firebaseio.com/')
 
 # Caminho no banco de dados 
 path = 'states/'
 
 # Definindo as portas que estão sendo utilizadas 
 # Led
-led = machine.PWM(machine.Pin(2), freq=1000)
+led = machine.PWM(machine.Pin(15))
 # Botão
-btn = machine.Pin(4, machine.Pin.IN)
+#btn = machine.Pin(4, machine.Pin.IN)
+btn = 1
 # Potenciômentro
 pot = machine.ADC(0)
 
-# função para controlar os pulsos de intensidade
-def pulse(l, t):
-    for i in range(20):
-        l.duty(int(math.sin(i / 10 * math.pi) * 500 + 500))
-        time.sleep_ms(t)
 
 while True:
-    if(btn.value()):
-        pulse(led, 1024)
-        db.put(path, {'led': 'on'})
+    if(btn):
+        led.duty(1024)
+        fb.put(path, {'led': 'on'})
     else:
-        pulse(led, 0)
-        db.put(path, {'led': 'off'})
+        led.duty(0)
+        fb.put(path, {'led': 'off'})
 
-    if(btn.value()):
-        pulse(led, pot.read())
-        db.put(path, {'pot': pot.read()})
+    if(btn):
+        led.duty(pot.read())
+        fb.put(path, {'pot': pot.read()})
     else:
         pass
